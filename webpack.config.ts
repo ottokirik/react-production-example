573cf2c1c25b719d5163872877de37001c39464b
Petrov Kirill <ottokirik@gmail.com>
2022-09-11 23:47:08 +0300

    refactor: webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,21 @@
-import path from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { buildWebpackConfig } from './config/build';
+import { BuildMode, BuildPaths } from './config/build/types/config';
+import { resolve } from 'path';
+import { Configuration } from 'webpack';
 
-const config: webpack.Configuration = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['tsx', 'ts', 'jsx', 'js'],
-  },
+const paths: BuildPaths = {
+  entry: resolve(__dirname, 'src', 'index.ts'),
+  build: resolve(__dirname, 'build'),
+  html: resolve(__dirname, 'public', 'index.html'),
 };
+
+const mode: BuildMode = 'development';
+const isDev = mode === 'development';
+
+const config: Configuration = buildWebpackConfig({
+  mode,
+  paths,
+  isDev,
+});
 
 export default config;
